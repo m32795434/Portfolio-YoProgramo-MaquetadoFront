5c3df64e50c1ac8bb5497be265628091ceb0101a
Manuel Augusto Bravard <manubravard@gmail.com>
2022-11-22 22:28:45 -0300

    everything ready to work with Bootstrap and Parcel

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+import bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap.min';//.js too
+/*
+const popers = document.querySelectorAll('[data-bs-toggle="popover"]');
+Array.from(popers).forEach((pop)=>new bootstrap.Popover(pop));
+
+/*
+import '../../node_modules/bootstrap/js/dist/button';
+
+import popover from '../../node_modules/bootstrap/js/dist/popover.js';
+const popers = document.querySelectorAll('[data-bs-toggle="popover"]');
+Array.from(popers).forEach((pop)=>new popover(pop));
+*/
+
+/*
+import {Popover} from 'bootstrap';
+const popers = document.querySelectorAll('[data-bs-toggle="popover"]');
+Array.from(popers).forEach((pop)=>{
+    new Popover(pop)
+})
+*/
