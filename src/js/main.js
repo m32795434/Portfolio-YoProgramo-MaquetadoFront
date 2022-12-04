811bb980e53c608e50382492af596cf8be79e29c
Manuel Augusto Bravard <manubravard@gmail.com>
2022-12-04 17:08:36 -0300

    index bug

--- src/js/main.js
+++ src/js/main.js
@@ -21,11 +21,11 @@ Array.from(popers).forEach((pop)=>{
 
 const imgHome = document.querySelector('main>img');
 if (window.visualViewport.width >= 768) {
-  imgHome.src = './home-md-1920.c416b6a5.png';
+  imgHome.src = '/home-md-1920.png';
 }
 function handleResize(e) {
   if (e.currentTarget.visualViewport.width >= 768) {
-    imgHome.src = './home-md-1920.c416b6a5.png';
+    imgHome.src = '/home-md-1920.png';
   } else {
     imgHome.src = '/home-mobile.41469544.png';
   }
