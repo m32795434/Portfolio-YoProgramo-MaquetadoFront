ec8a6b2233df4c2be1617a3ce25f5487beadc00d
Manuel Augusto Bravard <manubravard@gmail.com>
2022-12-04 16:24:37 -0300

    some button style & some Js to change the images

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,16 @@ Array.from(popers).forEach((pop)=>{
     new Popover(pop)
 })
 */
+
+const imgHome = document.querySelector('main>img');
+if (window.visualViewport.width >= 768) {
+  imgHome.src = './home-md-1920.c416b6a5.png';
+}
+function handleResize(e) {
+  if (e.currentTarget.visualViewport.width >= 768) {
+    imgHome.src = './home-md-1920.c416b6a5.png';
+  } else {
+    imgHome.src = '/home-mobile.41469544.png';
+  }
+}
+window.addEventListener('resize', handleResize);
