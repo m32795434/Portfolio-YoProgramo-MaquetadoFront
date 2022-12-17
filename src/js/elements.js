8bba7f96a95461835b082fba1d2ec6740323dedb
Manuel Augusto Bravard <manubravard@gmail.com>
2022-12-17 18:48:52 -0300

    selecting and storing a new image is working on Home page

--- src/js/elements.js
+++ src/js/elements.js
@@ -1,9 +1,10 @@
 const modalInner = document.querySelector('.modal-inner');
 const modalOuter = document.querySelector('.modal-outer');
-const editableElements = Array.from(document.querySelectorAll('.edit-text'));
+const editableElements = Array.from(document.querySelectorAll('.edit-content'));
 const editButtons = Array.from(document.querySelectorAll('.edit-button'));
 const loginButtons = Array.from(document.querySelectorAll('.login'));
 const changeImgInput = document.querySelector('#changeImg');
+const imgsToChange = Array.from(document.querySelectorAll('.imgsToChange'));
 export {
   modalInner,
   modalOuter,
@@ -11,4 +12,5 @@ export {
   editButtons,
   loginButtons,
   changeImgInput,
+  imgsToChange,
 };
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
-import { editableElements } from './elements';
+import { editableElements, imgsToChange } from './elements';
 
 let reducedEditables;
 
@@ -8,17 +8,23 @@ function wait(ms) {
   });
 }
 
-function editableTextsReducer(arr) {
-  const reduced = arr.reduce((textObj, currentEl) => {
-    textObj[currentEl.id] = currentEl.innerText;
-    return textObj;
+function editableContentsReducer(arr) {
+  const reduced = arr.reduce((contentObj, currentEl) => {
+    if (currentEl.tagName === 'IMG') {
+      console.log(`saving the content of an IMG`);
+      contentObj[currentEl.id] = currentEl.src;
+    } else if (currentEl.tagName === 'P' || currentEl.tagName.includes('H')) {
+      console.log(`saving the content of a ${currentEl.tagName}`);
+      contentObj[currentEl.id] = currentEl.innerText;
+    }
+    return contentObj;
   }, {});
   return reduced;
 }
 
 function mirrorToLocalStorage() {
   const { title } = document;
-  reducedEditables = editableTextsReducer(editableElements);
+  reducedEditables = editableContentsReducer(editableElements);
   console.log(title);
   switch (title) {
     case 'Home':
@@ -33,27 +39,45 @@ function mirrorToLocalStorage() {
 }
 function restoreFromLStorage() {
   const { title } = document;
-  const elsInnerText = JSON.parse(localStorage.getItem(title));
-  if (!elsInnerText) {
+  const elsContent = JSON.parse(localStorage.getItem(title));
+  if (!elsContent) {
     mirrorToLocalStorage();
   } else {
     editableElements.forEach((el) => {
-      el.innerText = elsInnerText[el.id];
+      if (el.tagName === 'IMG') {
+        console.log(`restoring the content of an IMG`);
+        el.src = elsContent[el.id];
+      } else if (el.tagName === 'P' || el.tagName.includes('H')) {
+        console.log(`restoring the content of a ${el.tagName}`);
+        el.innerText = elsContent[el.id];
+      }
     });
   }
 }
 async function selectImg(el) {
-  await wait(5000);
   const file = el.files[0];
-  console.log(file);
-  // const reader = new FileReader();
-  // reader.readAsText(file);
-  // reader.onload = function () {
-  // const { result } = reader;
-  // console.log(result);
-  // };
-  // reader.onerror = function () {
-  //   console.log(reader.erorr);
-  // };
+  let img;
+  console.log(file); // the File object
+  // select the img element
+  if (window.visualViewport.width >= 992) {
+    img = imgsToChange.find((elm) => elm.matches('.desktop'));
+  } else {
+    img = imgsToChange.find((elm) => elm.matches('.mobile'));
+  }
+  console.log(img);
+  // work on the reader
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    const { result } = reader;
+    console.log(e); // the ProgressEvent, type = "load"// we have loadstart, load, loadend, progress, error, abort
+    console.log(e.target); // reader --> FileReader
+    console.log(e.target.result); // the data
+    img.src = result;
+    // reducedEditables[img.id] = result;
+  };
+  reader.readAsDataURL(file);
+  reader.onerror = function () {
+    console.log(reader.erorr);
+  };
 }
 export { restoreFromLStorage, mirrorToLocalStorage, selectImg };
