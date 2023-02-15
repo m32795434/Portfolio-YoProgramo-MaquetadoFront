5c32e2549f3919068d4a5a434cbc100d2fd4a239
Manuel Augusto Bravard <manubravard@gmail.com>
2023-02-15 01:03:21 -0300

    some new tooltips+spelling check+style bug fix

--- src/js/experience.js
+++ src/js/experience.js
@@ -2,7 +2,7 @@ import 'bootstrap/js/dist/dropdown.js';
 import { Slider1 } from './slider.js';
 import { manageLogin } from './loguin.js';
 import {
-  cleanTooltipsFunct,
+  createTooltipsFunct,
   imgEventHandler,
   restoreFromLStorage,
 } from './utils';
@@ -41,4 +41,4 @@ imgClick.forEach((el) => {
 });
 
 // --------------------------TOOLTIPS--------------------------
-cleanTooltipsFunct();
+createTooltipsFunct();
--- src/js/projects.js
+++ src/js/projects.js
@@ -6,7 +6,7 @@ import {
   initConverter,
   imgEventHandler,
   checkForToasts,
-  cleanTooltipsFunct,
+  createTooltipsFunct,
 } from './utils';
 import { write } from './typer';
 import { imgClick } from './elements.js';
@@ -133,4 +133,4 @@ imgClick.forEach((el) => {
 });
 
 // --------------------------TOOLTIPS--------------------------
-cleanTooltipsFunct();
+createTooltipsFunct();
