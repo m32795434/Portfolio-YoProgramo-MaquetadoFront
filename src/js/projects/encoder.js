99ad71421670b6f2ae8cb8a9c38ef3c6fd927eb8
Manuel Augusto Bravard <manubravard@gmail.com>
2023-02-23 11:40:08 -0300

    to do

--- src/js/projects/encoder.js
+++ src/js/projects/encoder.js
@@ -5,18 +5,16 @@ const filterInputs = Array.from(
 const decodeTitle = document.querySelector('#decodeTitle');
 
 /* eslint-disable */
-const encodeDic = ['a','b','1','c','d','2','e','f','g','3','h','i','8','j','k','9','l','m','4','n','o','0','p','q','r','5','s','t','u','6','v','w','7','x','y','z'];
+// const encodeDic = ['a','b','1','c','d','2','e','f','g','3','h','i','8','j','k','9','l','m','4','n','o','0','p','q','r','5','s','t','u','6','v','w','7','x','y','z'];
 const funkyLetters = {
   '-': '₋', '!': 'ᵎ', '?': 'ˀ', '(': '⁽', ')': '₎', '+': '⁺', '=': '₌', '0': '⁰', '1': '₁', '2': '²', '4': '₄', '5': '₅', '6': '₆', '7': '⁷', '8': '⁸', '9': '⁹', a: 'ᵃ', A: 'ᴬ', B: 'ᴮ', b: 'ᵦ', C: '𝒸', d: 'ᵈ', D: 'ᴰ', e: 'ₑ', E: 'ᴱ', f: '𝒻', F: 'ᶠ', g: 'ᵍ', G: 'ᴳ', h: 'ʰ', H: 'ₕ', I: 'ᵢ', i: 'ᵢ', j: 'ʲ', J: 'ᴶ', K: 'ₖ', k: 'ₖ', l: 'ˡ', L: 'ᴸ', m: 'ᵐ', M: 'ₘ', n: 'ₙ', N: 'ᴺ', o: 'ᵒ', O: 'ᴼ', p: 'ᵖ', P: 'ᴾ', Q: 'ᵠ', q: 'ᑫ', r: 'ʳ', R: 'ᵣ', S: 'ˢ', s: 'ˢ', t: 'ᵗ', T: 'ₜ', u: 'ᵘ', U: 'ᵤ', v: 'ᵛ', V: 'ᵥ', w: '𝓌', W: 'ʷ', x: 'ˣ', X: 'ˣ', y: 'y', Y: 'Y', z: '𝓏', Z: 'ᶻ' };
 /* eslint-enable */
 const filters = {
   AlfaNumeric(letter) {
-    // check if there is a funky letter for this case or
-    // check if there is a lower case version
-    let funkyLetter = funkyLetters[letter];
-    funkyLetter = funkyLetter || funkyLetters[letter.toLowerCase()];
-    // if there ir nothing return the letter
-    return funkyLetter || letter;
+    // String.prototype.codePointAt();
+    // String.fromCharCode()
+    // take a pass. Use it to convert the text.
+    // One of the chars introduced in the text + one of the chars introduced in the pass => grater than 10175 => "introduce another char in the pass"
   },
   funky(letter) {
     // check if there is a funky letter for this case or
